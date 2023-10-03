ac169a6bf055eef12f302bfed651545900b64c5f
GloriaFlores03 <gloria.flores.ceb@immune.institute>
2023-10-03 18:54:38 +0200

    primeros arreglos del settings/admin/urls

--- tienda_de_chuches/tienda_de_chuches/settings.py
+++ tienda_de_chuches/tienda_de_chuches/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&6#k6-$2_r@=^jcaf1elb4klxh!v&q@4=qdv+79ud&s#f7gi)a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalogo.apps.CatalogoConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL='/catalogo/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'catalogo/media/')
